f4bb573eacca1081fc6b92f3e4589cea2068d75f
mariana ocireu <99839123+marisouza31@users.noreply.github.com>
2023-05-16 10:39:22 -0300

    Update conex.php

--- pw 22 5/conex.php
+++ pw 22 5/conex.php
@@ -8,7 +8,7 @@
     define('MYSQL_HOST' , 'localhost:3306');
     define('MYSQL_USER' , 'root');
     define('MYSQL_PASSWORD' , '');
-    define('MYSQL_DB_NAME' , 'agendamento_clientes');
+    define('MYSQL_DB_NAME' , 'cadastro');
  
     try {
         $pdo = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME, MYSQL_USER, MYSQL_PASSWORD);
